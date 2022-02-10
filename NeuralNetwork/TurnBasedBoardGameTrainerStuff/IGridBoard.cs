8fa7c94918885b6474061ad76e06245a3c524ff9
Mikah <mikah@mltd.com>
2022-02-09 16:14:19 -0800

    added even more things

--- NeuralNetwork/TurnBasedBoardGameTrainerStuff/IGridBoard.cs
+++ NeuralNetwork/TurnBasedBoardGameTrainerStuff/IGridBoard.cs
@@ -10,12 +10,11 @@ namespace NeuralNetwork.TurnBasedBoardGameTrainerStuff
         where TState : INetInputer 
         where TSquare : IGridSquare<TState>
     {
-        bool test { get; set; }
         TSquare[][] CurrentGame { get; }
-        //int YLength => CurrentGame?.Length ?? -1;
-        //int XLength => CurrentGame?[0]?.Length ?? -1;
+        int YLength => CurrentGame?.Length ?? -1;
+        int XLength => CurrentGame?[0]?.Length ?? -1;
         Players PreviousPlayer { get; }
-        //Players NextPlayer => GetNextPlayer[PreviousPlayer](this);
+        Players NextPlayer => GetNextPlayer[PreviousPlayer](this);
         bool IsTerminal { get; }        
         TSquare this[int y, int x] { get; set; }
         Dictionary<Players, Func<IGridBoard<TState, TSquare>, Players>> GetNextPlayer { get; }
