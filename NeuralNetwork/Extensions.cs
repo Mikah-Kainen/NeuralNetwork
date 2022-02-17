d5ea32de3162ac10354768114b51c43e72518c44
Mikah <mikah@mltd.com>
2022-02-16 16:41:41 -0800

    Added RandomMove extension function

--- NeuralNetwork/Extensions.cs
+++ NeuralNetwork/Extensions.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NeuralNetwork.TurnBasedBoardGameTrainerStuff;
+using NeuralNetwork.TurnBasedBoardGameTrainerStuff.Enums;
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +13,32 @@ namespace NeuralNetwork
         public static double NextDouble(this Random random, double min, double max)
         {
             return random.NextDouble() * (max - min) + min;
+        }
+
+        public static (int, int) RandomMove<TState, TSquare>(this IGridBoard<TState, TSquare> currentGame, Random random)
+            where TState : INetInputer
+            where TSquare : IGridSquare<TState>
+        {
+            var currentBoard = currentGame;
+
+            int returnY = random.Next(0, currentBoard.YLength);
+            int returnX = random.Next(0, currentBoard.XLength);
+
+            while (currentBoard[returnY, returnX].Owner != Players.None)
+            {
+                returnX++;
+                if (returnX >= currentBoard.XLength)
+                {
+                    returnX = 0;
+                    returnY++;
+                    if (returnY >= currentBoard.YLength)
+                    {
+                        returnY = 0;
+                    }
+                }
+            }
+
+            return (returnY, returnX);
         }
     }
 }
